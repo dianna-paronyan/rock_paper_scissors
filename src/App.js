670c072838a13cde1b2068e3364deb2c801753ef
dianna1234 <01234dianna@gmail.com>
2022-03-10 01:24:50 +0400

    rock_paper_scissors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+const rock = 'images/rock_.jpg';
+const paper = 'images/paper_.jpg';
+const scissors = 'images/scissors_.jpg';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [userChoice, setUserChoice] = useState(null);
+    const [computerChoice, setComputerUserChoice] = useState(null);
+    const [result, setResult] = useState(null);
+    const [show, setShow] = useState(true);
+    const imgChoices = [rock, paper, scissors];
+    
+  
+    function handleImgClick(imgSrc){
+        setUserChoice(imgSrc);
+        generateComputerChoices()
+    }
+
+    function generateComputerChoices(){
+        const randomChoice = imgChoices[Math.floor(Math.random()*imgChoices.length)];
+        setComputerUserChoice(randomChoice);
+        
+    }
+
+    useEffect(()=>{
+        results();
+        setShow(false)
+    }, [userChoice, computerChoice])
+
+
+    function results(){
+        const txt = userChoice+computerChoice;
+        if(txt  === `${paper+rock}` || txt === `${rock+scissors}`
+        || txt === `${scissors+paper}` ){
+            return setResult('You Win');
+        }
+        if(txt === `${rock+paper}` || txt=== `${scissors+rock}` 
+        || txt === `${paper+scissors}`){
+          return setResult('You Lose');
+        }
+        if(txt === `${rock+rock}`|| txt === `${scissors+scissors}`
+        || txt === `${paper+paper}` ){
+          return setResult(`It's draw`);
+        }
+        // console.log(userChoice, computerChoice);
+    }
+
+    return (
+      <>
+        <h1>Rock Paper Scissors</h1>
+
+        <div className="App">
+
+            {imgChoices.map((imgSrc, index)=> <img className='img' src={imgSrc} key={index} 
+            onClick={()=> handleImgClick(imgSrc)} style={{ marginLeft:'20px'}} /> )}
+
+        </div>
+
+        <div className='box2'>
+            <div className='first'>
+
+              <h3>Your choice is  </h3>
+              {show ?  <img className='img' /> : <img  src={userChoice} />}
+
+            </div>
+
+            <div className='second'>
+
+              <h3>Computer choice is</h3>
+              {show ? <img  className='img' src={computerChoice} /> : <img src={computerChoice} /> }
+
+            </div>
+
+        </div>
+
+        <h2>{result}</h2>
+      </>
+    );
 }
 
 export default App;
